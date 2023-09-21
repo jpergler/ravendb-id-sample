0594d2ad83fa182c0536bbcc765ca2925c96fe68
Jindřich Pergler <jindrich@pergler.eu>
2023-09-21 11:03:32 +0200

    Add comments

--- RavenIdConventionSample/Program.cs
+++ RavenIdConventionSample/Program.cs
@@ -17,9 +17,12 @@ await session.StoreAsync(item);
 await session.SaveChangesAsync();
 
 Console.WriteLine($"Item id prefix is '{item.Id.Split('/')[0]}'");
+// When cloning this document in database it prefills the prefix with first character uppercase ("Item/" in this case)
 
 var multiWordItem = MultiWordItem.New("multiple words in document type name");
 await session.StoreAsync(multiWordItem);
 await session.SaveChangesAsync();
 
 Console.WriteLine($"MultiWordItem id prefix is '{multiWordItem.Id.Split('/')[0]}'");
+// When cloning this document in database it prefills the prefix with first character uppercase ("MultiWordItem/" in this case)
+// but in this case it is the same as the default convention in RavenDB.Client 
